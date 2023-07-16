31b5a44678389b5d445fd4bf57be6330dc9eac40
hellnomary <maria.porcires@outlook.com>
2023-07-16 13:10:35 +0200

    setup

---
+++ react-counter/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <div className='counter'>
+
+      <button className='counter--minus'></button>
+      <section className='counter--count'>
+       <h1>1</h1>
+      </section>
+      <button className='counter--plus'></button>
+      
+  
+    </div>
+  )
+}
+
+export default App
