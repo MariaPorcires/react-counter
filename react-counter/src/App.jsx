19b68d7ae906e99380bb8b13b681a7a95167b1f0
hellnomary <maria.porcires@outlook.com>
2023-07-16 13:25:04 +0200

    css

--- react-counter/src/App.jsx
+++ react-counter/src/App.jsx
@@ -3,16 +3,24 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  
+  const [counter, setCount] = useState(0)
+
+  function plus() {
+    setCount (prevCount => prevCount + 1)
+  }
+
+  function minus() {
+    setCount (prevCount => prevCount -1)
+  }
 
   return (
     <div className='counter'>
 
-      <button className='counter--minus'></button>
+      <button className='counter--minus' onClick={minus}>-</button>
       <section className='counter--count'>
-       <h1>1</h1>
+        <h1>{counter}</h1>
       </section>
-      <button className='counter--plus'></button>
+      <button className='counter--plus' onClick={plus}>+</button>
       
   
     </div>
@@ -20,3 +28,4 @@ function App() {
 }
 
 export default App
+
